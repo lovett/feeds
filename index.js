0ade292c2d8bd4ffda11fa39360c3ce77658b247
Bill Lovett <bill@ilovett.com>
2013-12-26 16:54:46 -0500

    Restructure the /entries endpoint to support pagination. Serve the index to all suburls of /pages to support pushState.

--- index.js
+++ index.js
@@ -1,33 +1,43 @@
 var world = require('./world');
 var restify = require('restify');
+var fs = require('fs');
 var server = restify.createServer();
 
 server.use(restify.queryParser({ mapParams: false }));
 server.use(restify.gzipResponse());
 
-server.get('/', restify.serveStatic({
-  'directory': './static/',
-  'default': 'index.html'
-}));
-
 // display elements from from entries:queued
 server.get('/entries/', function unreadEntries (request, response, next) {
-    var page = Math.abs(parseInt(request.query.page, 10)) || 0;
-    var page_size = Math.abs(parseInt(request.query.page_size, 10))  || 10;
+    var page = Math.abs(request.query.page) || 1;
+    var page_size = Math.min(Math.abs(request.query.page_size) || 10, 50);
     var start = (page - 1) * page_size;
     var end = start + page_size - 1;
 
     world.client.lrange('entries:queued', start, end, function (err, ids) {
         var multi = world.client.multi();
 
+        // total entries
+        multi.llen('entries:queued', function (err, result) {
+            return result;
+        });
+
+        // entry list
         ids.forEach(function (id) {
             multi.hgetall('entry:' + id, function (err, entry) {
                 return entry;
             });
         });
 
-        multi.exec(function (err, entries) {
-            response.send(entries);
+        multi.exec(function (err, result) {
+            var total_entries = result.shift();
+            response.send({
+                entry_count: total_entries,
+                page: page,
+                page_count: Math.ceil(total_entries / page_size),
+                page_size: page_size,
+                page_start: start,
+                entries: result
+            });
         });
     });
     return next();
@@ -38,6 +48,30 @@ server.post('/entries/read', function readEntry (request, response, next) {
     return next();
 });
 
+
+server.get('/page/.*', function indexHTML(req, res, next) {
+    fs.readFile(__dirname + '/static/index.html', function (err, data) {
+        if (err) {
+            next(err);
+            return;
+        }
+        
+        res.setHeader('Content-Type', 'text/html');
+        res.writeHead(200);
+        res.end(data);
+        next();    
+    });
+});
+    
+
+server.get('/.*', restify.serveStatic({
+  'directory': './static/',
+  'default': 'index.html'
+}));
+
+
+
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
+
