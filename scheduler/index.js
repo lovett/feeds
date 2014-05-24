61912597056734dcb4f4843657a9483e44608420
Bill Lovett <bill@ilovett.com>
2014-05-23 21:43:57 -0400

    Adding a script to manage scheduling of feed collection

---
+++ scheduler/index.js
@@ -0,0 +1,46 @@
+var world = require("../world");
+var redis = require("redis");
+
+var main = function () {
+    var now = new Date().getTime();
+    world.client.zrangebyscore([world.keys.feedSubscriptions, 1, '+inf'],  function (err, ids) {
+        if (err) throw err;
+
+        var multi = world.client.multi();
+
+        var minMS = 1 * 60 * 60 * 1000;
+
+        ids.forEach(function (id) {
+            var key = world.keys.feed(id);
+            
+            multi.hmget([key, 'lastUpdate', 'nextCheck'], function (err, result) {
+                var lastUpdate = parseInt(result.shift(), 10) || 0;
+                var nextCheck = parseInt(result.shift(), 10) || 0;
+
+                if (nextCheck > now) {
+                    console.log(id + ' does not need to be checked for another ' + (nextCheck - now) + ' ms');
+                    return;
+                }
+
+                if (lastUpdate > now - minMS) {
+                    console.log(id + ' was recently updated, and does not to be checked');
+                    world.client.hset(key, 'nextCheck', lastUpdate + minMS); 
+                    return;
+                }
+
+                console.log('Fetching ' + id);
+
+                world.client.publish('feed:fetch', id, function (err, received) {
+                    world.client.hset(key, 'nextCheck', now + minMS); 
+                });
+            });
+        });
+
+        multi.exec(function (err, result) {
+            setTimeout(main, 5 * 1000);
+        });
+
+    });
+}
+
+main();
