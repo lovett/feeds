31997fffb2e6792fd9483ab207d27fad7aa1b9ae
Bill Lovett <bill@ilovett.com>
2014-11-17 23:32:41 -0500

    Adjusted url normalization to account for encoded characters and trailing slashes

--- world.js
+++ world.js
@@ -61,10 +61,23 @@ module.exports = {
         crypto.pbkdf2(password, salt, 10000, 512, callback);
     },
     normalizeUrl: function (url) {
-        var norm = url.toLowerCase();
-        norm = norm.replace(/^https?:\/\//, '');
+        var norm = url.toLowerCase().trim();
+
+        // convert escaped characters
+        norm = unescape(norm)
+
+        // remove the scheme
+        norm = norm.replace(/^[a-z]+:\/\//, '');
+
+        // remove the www subdomain
         norm = norm.replace(/^www\./, '');
+
+        // remove the querystring
         norm = norm.replace(/\?.*/, '')
+
+        // remove the trailing slash
+        norm = norm.replace(/\/$/, '')
+
         return norm;
     },
     hash: function (key) {
