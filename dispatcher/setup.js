c6896ed26fbea6eb1a9e3c3b49a38d47634b9702
Bill Lovett <bill@ilovett.com>
2015-07-16 11:53:52 -0400

    Switch entry storage from redis to sqlite
    To enforce foreign key constraints, set a pragma during setup.

--- dispatcher/setup.js
+++ dispatcher/setup.js
@@ -6,10 +6,11 @@ module.exports = function (db) {
     });
     
     db.serialize(function () {
+        db.run('PRAGMA foreign_keys=1');
         db.run('CREATE TABLE IF NOT EXISTS feeds (id INTEGER PRIMARY KEY, url TEXT NOT NULL, siteUrl TEXT, nextFetchUtc DEFAULT CURRENT_TIMESTAMP)');
         db.run('CREATE UNIQUE INDEX IF NOT EXISTS feed_url ON feeds (url)');
 
-        db.run('CREATE TABLE IF NOT EXISTS entries (id INTEGER PRIMARY KEY, feedId INTEGER, url TEXT, title TEXT, createdUtc DEFAULT CURRENT_TIMESTAMP, FOREIGN KEY(feedId) REFERENCES feeds(id))');
+        db.run('CREATE TABLE IF NOT EXISTS entries (id INTEGER PRIMARY KEY, feedId INTEGER NOT NULL, url TEXT NOT NULL, title TEXT NOT NULL, createdUtc DEFAULT CURRENT_TIMESTAMP, FOREIGN KEY(feedId) REFERENCES feeds(id))');
         db.run('CREATE UNIQUE INDEX IF NOT EXISTS entry_url ON entries (url)');
 
         db.run('CREATE TABLE IF NOT EXISTS discussions (id INTEGER PRIMARY KEY, entryId INTEGER, tally INTEGER DEFAULT 0, name TEXT, url TEXT, FOREIGN KEY (entryId) REFERENCES entries(id))');
---
+++ test/dispatcher/entry-store-spec.js
@@ -0,0 +1,121 @@
+var sqlite3 = require('sqlite3').verbose();
+var setup = require('../../dispatcher/setup');
+var entryStore = require('../../dispatcher/entry/store');
+var assert = require('assert');
+var events = require('events');
+
+describe('entry:store handler', function() {
+    beforeEach(function (done) {
+        var self = this;
+        this.db = new sqlite3.Database(':memory:');
+        this.emitter = new events.EventEmitter();
+        this.emitter.on('entry:store', entryStore);
+        this.emitter.on('setup', setup);
+        
+        this.emitter.on('setup:done', function () {
+            self.db.run('INSERT INTO feeds (url) VALUES (?)', ['http://example.com/feed.rss'], function (err) {
+                done();
+            });
+        });
+        
+        this.emitter.emit('setup', self.db);
+        
+    });
+
+    afterEach(function () {
+        this.db.close();
+        this.emitter.removeAllListeners();
+    });
+
+    it('adds a row to the entries table', function (done) {
+        var self, entry;
+
+        self = this;
+        entry = {
+            title: 'the title',
+            createdUtc: 1,
+            url: 'http://example.com/entry1.html'
+        };
+        
+        self.emitter.on('entry:store:done', function (changes, lastID) {
+            assert.strictEqual(changes, 1);
+            assert.strictEqual(lastID, 1);
+
+            self.db.get('SELECT COUNT(*) as count FROM entries', function (err, row) {
+                assert.strictEqual(err, null);
+                assert.strictEqual(row.count, 1);
+                done();
+            });
+        });
+
+        self.emitter.emit('entry:store', self.db, 1, entry);
+    });
+
+    it('blocks duplicate urls', function (done) {
+        var self, entry;
+
+        self = this;
+        entry = {
+            title: 'the title',
+            createdUtc: 1,
+            url: 'http://example.com/entry1.html'
+        };
+        
+        self.emitter.emit('entry:store', self.db, 1, entry);
+
+        self.emitter.once('entry:store:done', function (changes, lastID) {
+            assert.strictEqual(changes, 1);
+            assert.strictEqual(lastID, 1);
+            self.emitter.once('entry:store:done', function (changes, lastID) {
+                assert.strictEqual(changes, 0);
+                
+                self.db.get('SELECT COUNT(*) as count FROM entries', function (err, row) {
+                    assert.strictEqual(err, null);
+                    assert.strictEqual(row.count, 1);
+                    done();
+                });
+            });
+            self.emitter.emit('entry:store', self.db, 1, entry);
+        });
+    });
+
+    it('requires entry url', function (done) {
+        var self, entry;
+
+        self = this;
+        entry = {}
+        
+        self.emitter.on('log:warn', function (message, fields) {
+            assert(message);
+            assert(fields);
+            
+            self.db.get('SELECT COUNT(*) as count FROM entries', function (err, row) {
+                assert.strictEqual(row.count, 0);
+                done();
+            });
+        });
+
+        self.emitter.emit('entry:store', self.db, 1, entry);
+    });
+
+    it('requires valid feed ID', function (done) {
+        var self, entry;
+
+        self = this;
+        entry = {
+            url: 'http://example.com'
+        }
+        
+        self.emitter.on('entry:store:done', function (changes, lastID) {
+            assert.strictEqual(changes, undefined);
+            
+            self.db.get('SELECT COUNT(*) as count FROM entries', function (err, row) {
+                assert.strictEqual(row.count, 0);
+                done();
+            });
+        });
+
+        self.emitter.emit('entry:store', self.db, 999, entry);
+    });
+    
+});
