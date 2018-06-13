8d6e1fa8e586f3146ea771382a71251e8ea91a8c
Bill Lovett <bill@ilovett.com>
2018-06-12 22:32:52 -0400

    Preserve hash during URL normalization

--- util/normalize.js
+++ util/normalize.js
@@ -1,37 +1,51 @@
-var url = require('url');
+const url = require('url');
 
+/**
+ * Cleanup functions for bringing consistency to erratic values.
+*/
 module.exports = {
+    /**
+     * A normalized URL drops things that would throw off equality
+     * comparisons, such as case sensitivity, link protocol, and www
+     * subdomain.
+     *
+     * Normalized URLs should not be used as replacements. They are
+     * only for comparison.
+     */
     url: function (inputUrl) {
-        var cleaned, parsed, result;
+        var cleaned, parsedUrl, result;
 
         // pre-parse cleanup
         cleaned = inputUrl.toLowerCase().trim();
         cleaned = unescape(cleaned);
 
-        parsed = url.parse(cleaned, true);
+        parsedUrl = url.parse(cleaned, true, true);
 
         // discard unwanted fields
-        if (parsed.host) {
-            delete parsed.host;
+        if (parsedUrl.host) {
+            delete parsedUrl.host;
         }
-        parsed.hash = null;
-        parsed.search = null;
-        parsed.protocol = null;
+
+        if (parsedUrl.hash === '#') {
+            delete parsedUrl.hash;
+        }
+
+        delete parsedUrl.protocol;
 
         // remove the www subdomain
-        if (parsed.hostname) {
-            parsed.hostname = parsed.hostname.replace(/^www\./, '');
+        if (parsedUrl.hostname) {
+            parsedUrl.hostname = parsedUrl.hostname.replace(/^www\./, '');
         }
 
         // remove selected querystring vars
-        Object.keys(parsed.query).forEach(function (key) {
-            if (key.indexOf('utm_') === 0 || !parsed.query[key]) {
-                delete parsed.query[key];
+        Object.keys(parsedUrl.query).forEach(function (key) {
+            if (key.indexOf('utm_') === 0 || !parsedUrl.query[key]) {
+                delete parsedUrl.query[key];
             }
         });
 
         // convert back into a string
-        result = url.format(parsed);
+        result = url.format(parsedUrl);
         result = result.replace(/^\/\//, '');
         result = result.replace(/[=\/]$/, '');
         return result;
