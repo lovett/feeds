63ba04a8b0b5a995b4e1787d7dd942f418f1d997
Bill Lovett <bill@ilovett.com>
2018-07-23 21:32:20 -0400

    Add some rudimentary styling

--- server/static/headlines.js
+++ server/static/headlines.js
@@ -2,6 +2,7 @@ const SubscriptionViewModel = {
     adding: false,
     editing: false,
     newFeed: {},
+    activeId: 0,
     feeds: [],
     labels: {},
     fields: [],
@@ -94,27 +95,58 @@ const SubscriptionList = {
         node = m('header', SubscriptionViewModel.getLabel('group'));
         nodes.push(node);
 
+        let actions = [];
+
         // add link
-        node = m('p', m('a', {
+        node = m('a.add', {
             href: '#',
             hidden: SubscriptionViewModel.adding,
             onclick: (e) => {
                 e.preventDefault();
                 SubscriptionViewModel.adding = true;
             }
-        }, SubscriptionViewModel.getLabel('create')));
-        nodes.push(node);
+        }, SubscriptionViewModel.getLabel('create'));
+        actions.push(node);
 
         // cancel add
-        node = m('p', m('a', {
+        node = m('a.cancel-add', {
             href: '#',
             hidden: !SubscriptionViewModel.adding,
             onclick: (e) => {
                 e.preventDefault();
                 SubscriptionViewModel.adding = false;
             }
-        }, SubscriptionViewModel.getLabel('cancelAdd')));
-        nodes.push(node);
+        }, SubscriptionViewModel.getLabel('cancelAdd'));
+        actions.push(node);
+
+        // edit link
+        if (SubscriptionViewModel.hasFeeds()) {
+            node = m('a.edit', {
+                href: '#',
+                hidden: SubscriptionViewModel.editing,
+                onclick: (e) => {
+                    e.preventDefault();
+                    SubscriptionViewModel.editing = true;
+                }
+            }, SubscriptionViewModel.getLabel('edit'));
+            actions.push(node);
+
+            // cancel edit
+            node = m('a.cancel-edit', {
+                href: '#',
+                hidden: !SubscriptionViewModel.editing,
+                onclick: (e) => {
+                    e.preventDefault();
+                    SubscriptionViewModel.editing = false;
+                }
+            }, SubscriptionViewModel.getLabel('cancelEdit'));
+            actions.push(node);
+        } else {
+            node = m('p', SubscriptionViewModel.getLabel('empty'));
+            actions.push(node);
+        }
+
+        nodes.push(m('.actions', actions));
 
         // add form
         node = m(FeedForm, {
@@ -123,40 +155,12 @@ const SubscriptionList = {
         });
         nodes.push(node);
 
-        // edit link
-        if (SubscriptionViewModel.hasFeeds()) {
-            node = m('p', m('a', {
-                href: '#',
-                hidden: SubscriptionViewModel.editing,
-                onclick: (e) => {
-                    e.preventDefault();
-                    SubscriptionViewModel.editing = true;
-                }
-            }, SubscriptionViewModel.getLabel('edit')));
-            nodes.push(node);
-
-            // cancel edit
-            node = m('p', m('a', {
-                href: '#',
-                hidden: !SubscriptionViewModel.editing,
-                onclick: (e) => {
-                    e.preventDefault();
-                    SubscriptionViewModel.editing = false;
-                }
-            }, SubscriptionViewModel.getLabel('cancelEdit')));
-            nodes.push(node);
-        } else {
-            node = m('p', SubscriptionViewModel.getLabel('empty'));
-            nodes.push(node);
-        }
-
-
         // feed list
-        node = m('ul', SubscriptionViewModel.feeds.map((feed) => {
-            return m('li', m(SubscriptionListItem, {
+        node = m('ul.feeds', SubscriptionViewModel.feeds.map((feed) => {
+            return m(SubscriptionListItem, {
                 editing: SubscriptionViewModel.editing,
                 subscription: feed
-            }));
+            });
         }));
         nodes.push(node);
 
@@ -179,19 +183,16 @@ const SubscriptionListItem = {
             return vnode.state.asForm(vnode);
         }
 
-        return m('li', [
+        return m('li', {
+            class: (sub.id === SubscriptionViewModel.activeId ? 'active' : '')
+        }, [
             m('a.feed', {
                 href: `/feed/${sub.id}`,
                 title: sub.url,
+
                 oncreate: m.route.link
             }, sub.title),
-            m('span.entry-count', sub.entryCount),
-            m('a.site', {
-                href: sub.siteUrl,
-                hidden: !sub.siteUrl,
-                target: '_blank',
-                rel: 'external noopener noreferrer'
-            }, SubscriptionViewModel.getLabel('viewSite'))
+            m('span.entry-count', sub.entryCount)
         ]);
     }
 };
@@ -230,9 +231,6 @@ const EntryList = {
 
     view: function (vnode) {
         let nodes = [];
-
-        let node = m('header', FeedViewModel.getLabel('group'));
-        nodes.push(node);
 
         node = m('ul', FeedViewModel.entries.map(function (entry) {
             return m(EntryListItem, entry);
@@ -380,6 +378,7 @@ m.route(document.body, '/', {
 
     '/feed/:key': {
         render: (vnode) => {
+            SubscriptionViewModel.activeId = parseInt(vnode.attrs.key, 10);
             return m(Layout, [
                 m('section#entries', m(EntryList, vnode.attrs))
             ]);
