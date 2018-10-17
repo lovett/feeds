6752c929202360fa0df31a86b63cf1199a92744e
Bill Lovett <bill@ilovett.com>
2018-10-16 21:07:30 -0400

    Discard v1 client-side UI and miscellaneous utility scripts.

--- util/hash.js
+++
@@ -1,8 +0,0 @@
-var crypto = require('crypto');
-
-module.exports = function (key) {
-    key = key.toLowerCase();
-    var sha = crypto.createHash('sha1');
-    sha.update(key, 'utf8');
-    return sha.digest('hex');
-}
